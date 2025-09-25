e9db5563d78ef5557f6a60a97ca8bca14ede5661
giriprakash <prakashreddy0745@gmail.com>
2025-09-25 17:15:37 +0530

    added code

--- src/app.js
+++ src/app.js
@@ -3,4 +3,7 @@ const app= express()
 app.use("/test",(req,res)=>{
 res.send("hello from server from nodemon")
 })
+app.use("/hello",(req,res)=>{
+res.send("hello from server from new hello royte")
+})
 app.listen(3000,()=>console.log("server sttarted on 3000"))
